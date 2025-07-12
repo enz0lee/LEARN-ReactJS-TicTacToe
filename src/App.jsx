6e456113265774c29caf025ea986495ff210a3a4
Tai Le <tuantai0625@gmail.com>
2025-07-12 14:22:28 +0700

    refactor: extract some logics to different functions

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import Player from './components/Player'
 import Log from './components/Log'
 import GameOver from './components/GameOver'
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+}
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -54,21 +59,22 @@ function checkWinner(board, players) {
   return null
 }
 
+function deriveGameBoard(turns) {
+  const gameBoard = INITIAL_GAME_BOARD.map((row) => [...row])
+  for (const turn of turns) {
+    const { square, player } = turn
+    gameBoard[square.row][square.col] = player
+  }
+  return gameBoard
+}
 function App() {
-  const [players, setPlayers] = useState({
-    X: 'Player 1',
-    O: 'Player 2',
-  })
+  const [players, setPlayers] = useState(PLAYERS)
   // const [activePlayer, setActivePlayer] = useState('X')
   const [gameTurns, setGameTurns] = useState([])
   let activePlayer = deriveActivePlayer(gameTurns)
 
   // Create a copy of the initial game board
-  const gameBoard = initialGameBoard.map((row) => [...row])
-  for (const turn of gameTurns) {
-    const { square, player } = turn
-    gameBoard[square.row][square.col] = player
-  }
+  const gameBoard = deriveGameBoard(gameTurns)
 
   const winner = checkWinner(gameBoard, players)
   const isDraw = gameTurns.length === 9 && !winner
